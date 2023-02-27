38dfcde7eb4ca2a66a5e92e7721fc4cbc7af08be
Andrea66bit <njabuloandrea@gmail.com>
2023-02-27 09:36:26 -0800

    8-print_array.c

---
+++ 8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a: Array of integers
+ * @n: Number of elements of the array of integers
+ * Return: void
+ */
+
+void ptint-arrat(int *a, int n)
+
+{
+	
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+	printf("%d", a[j]);
+	if (j != (n - 1))
+	{
+	printf(",");
+	}
+	}
+	printf("\n");
+}	
