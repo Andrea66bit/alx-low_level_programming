f463ed3112cfb58765fd0079a278aeb974b62753
Andrea66bit <njabuloandrea@gmail.com>
2023-03-02 03:12:45 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,22 +2,26 @@
 
 int main(void)
 {
- int n;
- int a[5];
- int *p;
+int n;
+int a[5];
+int *p;
 
- a[2] = 1024;
- p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
+a[2] = 1024;
+p = &n;
+/**
+* _strncat - a function that concatenates two strings.
+* @dest: the destination string to be concatenated to
+* @src: the source string to concatenate
+* @n: the most bytes to concatenate to
+*
+* Description: it will use at most n bytes from src
+* and src does not need to be null-terminated if it contains n or more bytes
+*
+* Return: a pointer to the resulting string dest.
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
+
